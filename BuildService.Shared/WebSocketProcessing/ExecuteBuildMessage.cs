d7d0f38381db6b4667e8d059c89ab48a8409fd9e
kate <kate@dariox.club>
2022-05-17 21:44:08 +0800

    Actually start the build!

--- BuildService.Shared/WebSocketProcessing/ExecuteBuildMessage.cs
+++ BuildService.Shared/WebSocketProcessing/ExecuteBuildMessage.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using BuildService.Shared.Build;
 using BuildService.Shared.WebSocketService;
 
 namespace BuildService.Shared.WebSocketProcessing
@@ -9,10 +10,17 @@ namespace BuildService.Shared.WebSocketProcessing
     {
         public override void Process(Server server, WebSocketServerWrapper wrapper)
         {
-            
+            if (server.BuildController == null) return;
+            var buildable = server.BuildController.GetBuildable(target);
+            if (buildable == null) return;
+            var instance = new BuildInstance(server.BuildController, buildable);
+            instance.BuildMessageEvent += (sender, args) =>
+            {
+                wrapper.SendResponse(args);
+            };
+            instance.Start();
         }
 
-        public string organization { get; set; }
-        public string repository { get; set; }
+        public string target { get; set; }
     }
 }
