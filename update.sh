6db925577e7a33f420e237a7bbd1e52a628f8ca3
kate <kate@dariox.club>
2022-05-23 16:40:13 +0800

    Add update.sh

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://github.com/SixGrid/BuildService/releases/latest/download/BuildService.linux-amd64.zip
+unzip -o BuildService.linux-amd64.zip
+rm *.zip
+chmod +x BuildService
