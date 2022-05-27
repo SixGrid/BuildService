b8dfe5059a34f14149755985fd4aaeb35ff1a965
kate <kate@dariox.club>
2022-05-27 11:34:04 +0800

    Add database writing and reading

--- BuildService.Client.WinForms/Authentication/AuthenticationManager.cs
+++ BuildService.Client.WinForms/Authentication/AuthenticationManager.cs
@@ -4,13 +4,59 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using BuildServiceCommon.Helpers;
+
 namespace BuildService.Client.WinForms.Authentication
 {
     internal class AuthenticationManager
     {
         internal AuthenticationManager()
         {
-            
+            databaseDeserialize();
+        }
+
+        internal List<AuthenticationProfile> Profiles;
+        internal int DatabaseVersion;
+        internal bool InitialLoadComplete { get; private set; }
+
+        private string DATABASE_FILENAME = Path.Join(
+            Directory.GetCurrentDirectory(),
+            @"auth.db"
+        );
+
+        private void databaseDeserialize()
+        {
+            DatabaseHelper.Read(DATABASE_FILENAME, sr =>
+            {
+                DatabaseVersion = sr.ReadInt32();
+
+                Profiles = (List<AuthenticationProfile>)sr.ReadBList<AuthenticationProfile>();
+            }, () =>
+            {
+                MessageBox.Show(@"The local authentication database looks like it's corrupt. Oh well...", @"BuildService Client - Authentication Manager", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            });
+
+            databasePostProcess();
+        }
+        internal void DatabaseSerialize()
+        {
+            DatabaseHelper.Write(DATABASE_FILENAME, sw =>
+            {
+                sw.Write(DatabaseVersion);
+                sw.Write(Profiles);
+            });
+        }
+
+        private void databasePostProcess()
+        {
+            if (Profiles == null)
+            {
+                Profiles = new List<AuthenticationProfile>();
+                DatabaseVersion = BuildService.Client.WinForms.Program.VERSION;
+                InitialLoadComplete = true;
+                return;
+            }
+            InitialLoadComplete = true;
         }
     }
 }
