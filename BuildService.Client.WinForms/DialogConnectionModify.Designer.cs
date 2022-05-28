63403b79adcb730feb3c930d37d8c871bb78fc81
kate <kate@dariox.club>
2022-05-28 12:08:39 +0800

    Create form Dialog Connection Modify

---
+++ BuildService.Client.WinForms/DialogConnectionModify.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace BuildService.Client.WinForms
+{
+    partial class DialogConnectionModify
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DialogConnectionModify));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxDescription = new System.Windows.Forms.TextBox();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonDiscard = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(12, 27);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(318, 23);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Address";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(213, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Port";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.Location = new System.Drawing.Point(213, 83);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(117, 23);
+            this.textBoxPort.TabIndex = 5;
+            this.textBoxPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPort_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Description";
+            // 
+            // textBoxDescription
+            // 
+            this.textBoxDescription.Location = new System.Drawing.Point(12, 139);
+            this.textBoxDescription.Multiline = true;
+            this.textBoxDescription.Name = "textBoxDescription";
+            this.textBoxDescription.Size = new System.Drawing.Size(318, 92);
+            this.textBoxDescription.TabIndex = 7;
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Image = global::BuildService.Client.WinForms.Properties.Resources.bin;
+            this.buttonDelete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonDelete.Location = new System.Drawing.Point(255, 237);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 13;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            // 
+            // buttonDiscard
+            // 
+            this.buttonDiscard.Image = global::BuildService.Client.WinForms.Properties.Resources.cross;
+            this.buttonDiscard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonDiscard.Location = new System.Drawing.Point(93, 237);
+            this.buttonDiscard.Name = "buttonDiscard";
+            this.buttonDiscard.Size = new System.Drawing.Size(75, 23);
+            this.buttonDiscard.TabIndex = 12;
+            this.buttonDiscard.Text = "Discard";
+            this.buttonDiscard.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonDiscard.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Image = global::BuildService.Client.WinForms.Properties.Resources.tick;
+            this.buttonSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonSave.Location = new System.Drawing.Point(12, 237);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 11;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBoxAddress
+            // 
+            this.textBoxAddress.Location = new System.Drawing.Point(12, 83);
+            this.textBoxAddress.Name = "textBoxAddress";
+            this.textBoxAddress.Size = new System.Drawing.Size(195, 23);
+            this.textBoxAddress.TabIndex = 14;
+            // 
+            // DialogConnectionModify
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 273);
+            this.Controls.Add(this.textBoxAddress);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonDiscard);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxDescription);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DialogConnectionModify";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Build Service - Modify Connection";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBoxName;
+        private Label label2;
+        private Label label3;
+        private TextBox textBoxPort;
+        private Label label4;
+        private TextBox textBoxDescription;
+        private Button buttonDelete;
+        private Button buttonDiscard;
+        private Button buttonSave;
+        private TextBox textBoxAddress;
+    }
+}
---
+++ BuildService.Client.WinForms/DialogConnectionModify.cs
@@ -0,0 +1,45 @@
+﻿using BuildService.Client.WinForms.Network;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuildService.Client.WinForms
+{
+    public partial class DialogConnectionModify : Form
+    {
+        public ConnectionProfile TargetProfile;
+        public DialogConnectionModify(ConnectionProfile profile)
+        {
+            InitializeComponent();
+
+            TargetProfile = profile;
+
+            textBoxAddress.Text = profile.IpAddress;
+            textBoxPort.Text = profile.Port.ToString();
+
+            textBoxName.Text = profile.Name;
+            textBoxDescription.Text = profile.Description;
+        }
+
+        private void textBoxPort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            TargetProfile.IpAddress = textBoxAddress.Text;
+            TargetProfile.Port = Convert.ToUInt32(textBoxPort.Text);
+            TargetProfile.Name = textBoxName.Text;
+            TargetProfile.Description = textBoxDescription.Text;
+            TargetProfile.rUpdatedAt();
+            Program.ConnectionMan.DatabaseSerialize();
+        }
+    }
+}
