17be58ba80811562efc3b9e21be5d1148b4a6108
kate <kate@dariox.club>
2022-05-28 12:13:36 +0800

    Add View->Refresh to ToolStrip

--- BuildService.Client.WinForms/MainDashboard.Designer.cs
+++ BuildService.Client.WinForms/MainDashboard.Designer.cs
@@ -49,6 +49,10 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.profileListToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.connectionProfilesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.SuspendLayout();
@@ -75,9 +79,8 @@
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
-            this.splitContainer1.Size = new System.Drawing.Size(914, 456);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 426);
             this.splitContainer1.SplitterDistance = 203;
-            this.splitContainer1.SplitterWidth = 5;
             this.splitContainer1.TabIndex = 0;
             // 
             // splitContainer2
@@ -94,8 +97,8 @@
             // splitContainer2.Panel2
             // 
             this.splitContainer2.Panel2.Controls.Add(this.tableLayoutPanel2);
-            this.splitContainer2.Size = new System.Drawing.Size(203, 456);
-            this.splitContainer2.SplitterDistance = 226;
+            this.splitContainer2.Size = new System.Drawing.Size(203, 426);
+            this.splitContainer2.SplitterDistance = 210;
             this.splitContainer2.TabIndex = 2;
             // 
             // tableLayoutPanel1
@@ -111,7 +114,7 @@
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(203, 226);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(203, 210);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // listView_Auth
@@ -119,10 +122,10 @@
             this.listView_Auth.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView_Auth.FullRowSelect = true;
             this.listView_Auth.GridLines = true;
-            this.listView_Auth.Location = new System.Drawing.Point(3, 30);
+            this.listView_Auth.Location = new System.Drawing.Point(3, 28);
             this.listView_Auth.MultiSelect = false;
             this.listView_Auth.Name = "listView_Auth";
-            this.listView_Auth.Size = new System.Drawing.Size(197, 193);
+            this.listView_Auth.Size = new System.Drawing.Size(197, 179);
             this.listView_Auth.TabIndex = 0;
             this.listView_Auth.UseCompatibleStateImageBehavior = false;
             this.listView_Auth.View = System.Windows.Forms.View.Details;
@@ -139,7 +142,7 @@
             this.toolStrip_Auth.Location = new System.Drawing.Point(0, 0);
             this.toolStrip_Auth.Name = "toolStrip_Auth";
             this.toolStrip_Auth.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.toolStrip_Auth.Size = new System.Drawing.Size(203, 27);
+            this.toolStrip_Auth.Size = new System.Drawing.Size(203, 25);
             this.toolStrip_Auth.Stretch = true;
             this.toolStrip_Auth.TabIndex = 1;
             this.toolStrip_Auth.Text = "toolStrip1";
@@ -150,7 +153,7 @@
             this.toolStripButton_AuthAdd.Image = global::BuildService.Client.WinForms.Properties.Resources.user__plus;
             this.toolStripButton_AuthAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_AuthAdd.Name = "toolStripButton_AuthAdd";
-            this.toolStripButton_AuthAdd.Size = new System.Drawing.Size(23, 24);
+            this.toolStripButton_AuthAdd.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton_AuthAdd.Text = "toolStripButton_AuthAdd";
             // 
             // toolStripButton_AuthEdit
@@ -159,7 +162,7 @@
             this.toolStripButton_AuthEdit.Image = global::BuildService.Client.WinForms.Properties.Resources.user__pencil;
             this.toolStripButton_AuthEdit.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_AuthEdit.Name = "toolStripButton_AuthEdit";
-            this.toolStripButton_AuthEdit.Size = new System.Drawing.Size(23, 24);
+            this.toolStripButton_AuthEdit.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton_AuthEdit.Text = "toolStripButton_AuthEdit";
             // 
             // toolStripButton_AuthRemove
@@ -168,7 +171,7 @@
             this.toolStripButton_AuthRemove.Image = global::BuildService.Client.WinForms.Properties.Resources.user__minus;
             this.toolStripButton_AuthRemove.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_AuthRemove.Name = "toolStripButton_AuthRemove";
-            this.toolStripButton_AuthRemove.Size = new System.Drawing.Size(23, 24);
+            this.toolStripButton_AuthRemove.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton_AuthRemove.Text = "toolStripButton_AuthRemove";
             // 
             // tableLayoutPanel2
@@ -184,7 +187,7 @@
             this.tableLayoutPanel2.RowCount = 2;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(203, 226);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(203, 212);
             this.tableLayoutPanel2.TabIndex = 4;
             // 
             // toolStrip_Connections
@@ -200,7 +203,7 @@
             this.toolStrip_Connections.Location = new System.Drawing.Point(0, 0);
             this.toolStrip_Connections.Name = "toolStrip_Connections";
             this.toolStrip_Connections.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.toolStrip_Connections.Size = new System.Drawing.Size(203, 27);
+            this.toolStrip_Connections.Size = new System.Drawing.Size(203, 25);
             this.toolStrip_Connections.Stretch = true;
             this.toolStrip_Connections.TabIndex = 0;
             this.toolStrip_Connections.Text = "toolStrip2";
@@ -211,7 +214,7 @@
             this.toolStripButton_ConnectionAdd.Image = global::BuildService.Client.WinForms.Properties.Resources.server__plus;
             this.toolStripButton_ConnectionAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_ConnectionAdd.Name = "toolStripButton_ConnectionAdd";
-            this.toolStripButton_ConnectionAdd.Size = new System.Drawing.Size(23, 24);
+            this.toolStripButton_ConnectionAdd.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton_ConnectionAdd.Text = "toolStripButton_ConnectionAdd";
             // 
             // toolStripButton_ConnectionEdit
@@ -220,7 +223,7 @@
             this.toolStripButton_ConnectionEdit.Image = global::BuildService.Client.WinForms.Properties.Resources.server__pencil;
             this.toolStripButton_ConnectionEdit.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_ConnectionEdit.Name = "toolStripButton_ConnectionEdit";
-            this.toolStripButton_ConnectionEdit.Size = new System.Drawing.Size(23, 24);
+            this.toolStripButton_ConnectionEdit.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton_ConnectionEdit.Text = "toolStripButton_ConnectionEdit";
             // 
             // toolStripButton_ConnectionRemove
@@ -229,7 +232,7 @@
             this.toolStripButton_ConnectionRemove.Image = global::BuildService.Client.WinForms.Properties.Resources.server__minus;
             this.toolStripButton_ConnectionRemove.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_ConnectionRemove.Name = "toolStripButton_ConnectionRemove";
-            this.toolStripButton_ConnectionRemove.Size = new System.Drawing.Size(23, 24);
+            this.toolStripButton_ConnectionRemove.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton_ConnectionRemove.Text = "toolStripButton_ConnectionRemove";
             // 
             // toolStripButton_ConnectionConnect
@@ -239,7 +242,7 @@
             this.toolStripButton_ConnectionConnect.Image = global::BuildService.Client.WinForms.Properties.Resources.plug__arrow;
             this.toolStripButton_ConnectionConnect.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButton_ConnectionConnect.Name = "toolStripButton_ConnectionConnect";
-            this.toolStripButton_ConnectionConnect.Size = new System.Drawing.Size(23, 24);
+            this.toolStripButton_ConnectionConnect.Size = new System.Drawing.Size(23, 22);
             this.toolStripButton_ConnectionConnect.Text = "toolStripButton_ConnectionConnect";
             // 
             // listView_Connections
@@ -247,10 +250,10 @@
             this.listView_Connections.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listView_Connections.FullRowSelect = true;
             this.listView_Connections.GridLines = true;
-            this.listView_Connections.Location = new System.Drawing.Point(3, 30);
+            this.listView_Connections.Location = new System.Drawing.Point(3, 28);
             this.listView_Connections.MultiSelect = false;
             this.listView_Connections.Name = "listView_Connections";
-            this.listView_Connections.Size = new System.Drawing.Size(197, 193);
+            this.listView_Connections.Size = new System.Drawing.Size(197, 181);
             this.listView_Connections.TabIndex = 3;
             this.listView_Connections.UseCompatibleStateImageBehavior = false;
             this.listView_Connections.View = System.Windows.Forms.View.Details;
@@ -280,11 +283,11 @@
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
+            this.fileToolStripMenuItem,
+            this.viewToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(914, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
             this.menuStrip1.TabIndex = 1;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -300,15 +303,47 @@
             // 
             this.saveToolStripMenuItem.Image = global::BuildService.Client.WinForms.Properties.Resources.disk;
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.profileListToolStripMenuItem,
+            this.connectionProfilesToolStripMenuItem});
+            this.refreshToolStripMenuItem.Image = global::BuildService.Client.WinForms.Properties.Resources.arrow_circle;
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            // 
+            // profileListToolStripMenuItem
+            // 
+            this.profileListToolStripMenuItem.Image = global::BuildService.Client.WinForms.Properties.Resources.user;
+            this.profileListToolStripMenuItem.Name = "profileListToolStripMenuItem";
+            this.profileListToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.profileListToolStripMenuItem.Text = "Authentication Profiles";
+            // 
+            // connectionProfilesToolStripMenuItem
+            // 
+            this.connectionProfilesToolStripMenuItem.Image = global::BuildService.Client.WinForms.Properties.Resources.server;
+            this.connectionProfilesToolStripMenuItem.Name = "connectionProfilesToolStripMenuItem";
+            this.connectionProfilesToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.connectionProfilesToolStripMenuItem.Text = "Connection Profiles";
+            // 
             // MainDashboard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(914, 480);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -361,5 +396,9 @@
         private TableLayoutPanel tableLayoutPanel2;
         private ToolStripMenuItem fileToolStripMenuItem;
         private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem viewToolStripMenuItem;
+        private ToolStripMenuItem refreshToolStripMenuItem;
+        private ToolStripMenuItem profileListToolStripMenuItem;
+        private ToolStripMenuItem connectionProfilesToolStripMenuItem;
     }
 }
