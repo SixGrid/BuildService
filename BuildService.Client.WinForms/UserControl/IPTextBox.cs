8cbe6a40684a3e4beaf14f12f6561f3b646969cf
kate <kate@dariox.club>
2022-05-28 12:10:20 +0800

    Add control IPTextBox

---
+++ BuildService.Client.WinForms/UserControl/IPTextBox.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuildService.Client.WinForms.UserControl
+{
+    public partial class IPTextBox : TextBox
+    {
+        public IPTextBox() : base()
+        {
+            SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                var cp = base.CreateParams;
+                cp.ClassName = "SysIPAddress32";
+                return cp;
+            }
+        }
+    }
+}
