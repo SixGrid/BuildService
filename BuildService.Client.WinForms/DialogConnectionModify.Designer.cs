dfabfcb68298adddd26e860f08473a618da3b4fc
kate <kate@dariox.club>
2022-05-28 13:28:59 +0800

    Add heaps of logic (:

--- BuildService.Client.WinForms/DialogConnectionModify.Designer.cs
+++ BuildService.Client.WinForms/DialogConnectionModify.Designer.cs
@@ -40,12 +40,17 @@
             this.buttonDiscard = new System.Windows.Forms.Button();
             this.buttonSave = new System.Windows.Forms.Button();
             this.textBoxAddress = new System.Windows.Forms.TextBox();
+            this.checkBoxSecure = new System.Windows.Forms.CheckBox();
+            this.comboBoxAuthProfile = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxPath = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(14, 10);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(39, 15);
             this.label1.TabIndex = 0;
@@ -53,15 +58,15 @@
             // 
             // textBoxName
             // 
-            this.textBoxName.Location = new System.Drawing.Point(12, 27);
+            this.textBoxName.Location = new System.Drawing.Point(14, 29);
             this.textBoxName.Name = "textBoxName";
-            this.textBoxName.Size = new System.Drawing.Size(318, 23);
+            this.textBoxName.Size = new System.Drawing.Size(363, 23);
             this.textBoxName.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Location = new System.Drawing.Point(14, 69);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(49, 15);
             this.label2.TabIndex = 2;
@@ -70,7 +75,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(213, 65);
+            this.label3.Location = new System.Drawing.Point(243, 69);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(29, 15);
             this.label3.TabIndex = 4;
@@ -78,16 +83,16 @@
             // 
             // textBoxPort
             // 
-            this.textBoxPort.Location = new System.Drawing.Point(213, 83);
+            this.textBoxPort.Location = new System.Drawing.Point(243, 89);
             this.textBoxPort.Name = "textBoxPort";
-            this.textBoxPort.Size = new System.Drawing.Size(117, 23);
+            this.textBoxPort.Size = new System.Drawing.Size(133, 23);
             this.textBoxPort.TabIndex = 5;
             this.textBoxPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxPort_KeyPress);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 121);
+            this.label4.Location = new System.Drawing.Point(14, 197);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(67, 15);
             this.label4.TabIndex = 6;
@@ -95,43 +100,45 @@
             // 
             // textBoxDescription
             // 
-            this.textBoxDescription.Location = new System.Drawing.Point(12, 139);
+            this.textBoxDescription.Location = new System.Drawing.Point(14, 217);
             this.textBoxDescription.Multiline = true;
             this.textBoxDescription.Name = "textBoxDescription";
-            this.textBoxDescription.Size = new System.Drawing.Size(318, 92);
+            this.textBoxDescription.Size = new System.Drawing.Size(363, 98);
             this.textBoxDescription.TabIndex = 7;
             // 
             // buttonDelete
             // 
             this.buttonDelete.Image = global::BuildService.Client.WinForms.Properties.Resources.bin;
             this.buttonDelete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonDelete.Location = new System.Drawing.Point(255, 237);
+            this.buttonDelete.Location = new System.Drawing.Point(291, 321);
             this.buttonDelete.Name = "buttonDelete";
-            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.Size = new System.Drawing.Size(86, 25);
             this.buttonDelete.TabIndex = 13;
             this.buttonDelete.Text = "Delete";
             this.buttonDelete.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
             // 
             // buttonDiscard
             // 
             this.buttonDiscard.Image = global::BuildService.Client.WinForms.Properties.Resources.cross;
             this.buttonDiscard.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonDiscard.Location = new System.Drawing.Point(93, 237);
+            this.buttonDiscard.Location = new System.Drawing.Point(106, 321);
             this.buttonDiscard.Name = "buttonDiscard";
-            this.buttonDiscard.Size = new System.Drawing.Size(75, 23);
+            this.buttonDiscard.Size = new System.Drawing.Size(86, 25);
             this.buttonDiscard.TabIndex = 12;
             this.buttonDiscard.Text = "Discard";
             this.buttonDiscard.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.buttonDiscard.UseVisualStyleBackColor = true;
+            this.buttonDiscard.Click += new System.EventHandler(this.buttonDiscard_Click);
             // 
             // buttonSave
             // 
             this.buttonSave.Image = global::BuildService.Client.WinForms.Properties.Resources.tick;
             this.buttonSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.buttonSave.Location = new System.Drawing.Point(12, 237);
+            this.buttonSave.Location = new System.Drawing.Point(14, 321);
             this.buttonSave.Name = "buttonSave";
-            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.Size = new System.Drawing.Size(86, 25);
             this.buttonSave.TabIndex = 11;
             this.buttonSave.Text = "Save";
             this.buttonSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -140,16 +147,64 @@
             // 
             // textBoxAddress
             // 
-            this.textBoxAddress.Location = new System.Drawing.Point(12, 83);
+            this.textBoxAddress.Location = new System.Drawing.Point(14, 89);
             this.textBoxAddress.Name = "textBoxAddress";
-            this.textBoxAddress.Size = new System.Drawing.Size(195, 23);
+            this.textBoxAddress.Size = new System.Drawing.Size(222, 23);
             this.textBoxAddress.TabIndex = 14;
+            // 
+            // checkBoxSecure
+            // 
+            this.checkBoxSecure.AutoSize = true;
+            this.checkBoxSecure.Location = new System.Drawing.Point(101, 58);
+            this.checkBoxSecure.Name = "checkBoxSecure";
+            this.checkBoxSecure.Size = new System.Drawing.Size(91, 19);
+            this.checkBoxSecure.TabIndex = 15;
+            this.checkBoxSecure.Text = "Secure (wss)";
+            this.checkBoxSecure.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxAuthProfile
+            // 
+            this.comboBoxAuthProfile.FormattingEnabled = true;
+            this.comboBoxAuthProfile.Location = new System.Drawing.Point(14, 148);
+            this.comboBoxAuthProfile.Name = "comboBoxAuthProfile";
+            this.comboBoxAuthProfile.Size = new System.Drawing.Size(222, 24);
+            this.comboBoxAuthProfile.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 15);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Authentication Profile";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(243, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(31, 15);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Path";
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(243, 149);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(133, 23);
+            this.textBoxPath.TabIndex = 19;
             // 
             // DialogConnectionModify
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(342, 273);
+            this.ClientSize = new System.Drawing.Size(391, 358);
+            this.Controls.Add(this.textBoxPath);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxAuthProfile);
+            this.Controls.Add(this.checkBoxSecure);
             this.Controls.Add(this.textBoxAddress);
             this.Controls.Add(this.buttonDelete);
             this.Controls.Add(this.buttonDiscard);
@@ -189,5 +244,10 @@
         private Button buttonDiscard;
         private Button buttonSave;
         private TextBox textBoxAddress;
+        private CheckBox checkBoxSecure;
+        private ComboBox comboBoxAuthProfile;
+        private Label label5;
+        private Label label6;
+        private TextBox textBoxPath;
     }
 }
--- BuildService.Client.WinForms/DialogConnectionModify.cs
+++ BuildService.Client.WinForms/DialogConnectionModify.cs
@@ -1,4 +1,5 @@
-﻿using BuildService.Client.WinForms.Network;
+﻿using BuildService.Client.WinForms.Authentication;
+using BuildService.Client.WinForms.Network;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,29 +18,105 @@ namespace BuildService.Client.WinForms
         public DialogConnectionModify(ConnectionProfile profile)
         {
             InitializeComponent();
+            SetProfile(profile);
+        }
 
+        public void SetProfile(ConnectionProfile profile)
+        {
             TargetProfile = profile;
+
+            GenerateAuthProfileCombo();
 
             textBoxAddress.Text = profile.IpAddress;
             textBoxPort.Text = profile.Port.ToString();
 
             textBoxName.Text = profile.Name;
             textBoxDescription.Text = profile.Description;
+
+            textBoxPath.Text = profile.Path;
+
+            checkBoxSecure.Checked = profile.Secure;
+
+            int index = 0;
+            foreach (var item in comboBoxAuthProfile.Items)
+            {
+                var target = profile.GetAuthProfile();
+                if (target == null) continue;
+                if (item.ToString() == $@"{target.Label} ({target.ID})")
+                {
+                    comboBoxAuthProfile.SelectedIndex = index;
+                }
+                index++;
+            }
+        }
+
+        private void GenerateAuthProfileCombo()
+        {
+            comboBoxAuthProfile.Items.Clear();
+            foreach (var profile in Program.AuthenticationMan.Profiles)
+            {
+                comboBoxAuthProfile.Items.Add($@"{profile.Label} ({profile.ID})");
+            }
+        }
+
+        private AuthenticationProfile? GetSelectedAuthProfile()
+        {
+            foreach (var profile in Program.AuthenticationMan.Profiles)
+            {
+                if (comboBoxAuthProfile.SelectedItem.ToString() == $@"{profile.Label} ({profile.ID})")
+                {
+                    return profile;
+                }
+            }
+            return null;
         }
 
         private void textBoxPort_KeyPress(object sender, KeyPressEventArgs e)
         {
             e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            var target = Convert.ToInt64(textBoxPort.Text);
+            if (target > uint.MaxValue)
+            {
+                target = uint.MaxValue;
+            }
+            if (target < 0)
+            {
+                target = 0;
+            }
+
+            textBoxPort.Text = target.ToString();
         }
 
         private void buttonSave_Click(object sender, EventArgs e)
         {
+            TargetProfile.rUpdatedAt();
             TargetProfile.IpAddress = textBoxAddress.Text;
             TargetProfile.Port = Convert.ToUInt32(textBoxPort.Text);
             TargetProfile.Name = textBoxName.Text;
             TargetProfile.Description = textBoxDescription.Text;
-            TargetProfile.rUpdatedAt();
+            TargetProfile.Secure = checkBoxSecure.Checked;
+            TargetProfile.Path = textBoxPath.Text;
+            string? id = GetSelectedAuthProfile()?.ID;
+            TargetProfile.AuthProfileID = id == null ? @"" : id;
             Program.ConnectionMan.DatabaseSerialize();
+
+            Close();
+            Dispose();
+        }
+
+        private void buttonDiscard_Click(object sender, EventArgs e)
+        {
+            Close();
+            Dispose();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            Program.ConnectionMan.DeleteProfile(TargetProfile);
+            Program.ConnectionMan.DatabaseSerialize();
+
+            Close();
+            Dispose();
         }
     }
 }
